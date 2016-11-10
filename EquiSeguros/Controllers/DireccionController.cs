7d4d431751ebd01e1f2addff0c704e00af28e0e4
DFMERA <df.mera@gmail.com>
2016-11-10 16:08:41 -0500

    Direcciones

---
+++ EquiSeguros/Controllers/DireccionController.cs
@@ -0,0 +1,35 @@
+﻿using EquiSeguros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EquiSeguros.Controllers
+{
+    public class DireccionController : Controller
+    {
+        [HttpGet]
+        public ActionResult DireccionForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DireccionForm(Direccion direccion)
+        {
+            if (ModelState.IsValid)
+            {
+                //MarcaManager manager = new MarcaManager();
+                //manager.CrarMarca(marca);
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ EquiSeguros/Models/Direccion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EquiSeguros.Models
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+        public int IdPersona { get; set; }
+        public int TipoDireccion { get; set; }
+        public int Provincia { get; set; }
+        public int Canton { get; set; }
+        public string CallePrincipal { get; set; }
+        public string Callesecundaria { get; set; }
+        public string Numero { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
+    }
+}
