861d2d1e79b40eef72996c910e531fbc1a58f7ac
Karla Herdoiza <karla.herdoiza@gmail.com>
2016-11-10 16:06:26 -0500

    Forma Básica de Asegurados
    Primera versión

---
+++ EquiSeguros/Models/BussinesEntity/Asegurado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EquiSeguros.Models.BussinesEntity
+{
+    public class Asegurado
+    {
+        public int Id { get; set; }
+        public string Documento { get; set; }
+        public string  ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Nombres { get; set; }
+        public int  TipoDocumento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int EstadoCivil { get; set; }
+        public int TipoPersona { get; set; }
+    }
+}
