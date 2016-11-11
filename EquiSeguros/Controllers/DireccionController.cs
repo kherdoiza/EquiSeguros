19fb75a10ce50dd5480a3d8b1136d7d53e550054
DFMERA <df.mera@gmail.com>
2016-11-11 18:02:17 -0500

    direccion

--- EquiSeguros/Controllers/DireccionController.cs
+++ EquiSeguros/Controllers/DireccionController.cs
@@ -22,8 +22,28 @@ namespace EquiSeguros.Controllers
             pais1.CodPais = 2;
             pais1.TxtDescripcion = "Colombia";
             paises.Add(pais1);
-
             ViewBag.Paises = paises;
+
+            List<Provincia> provincias = new List<Provincia>();
+            Provincia provincia = new Provincia();
+            provincia.CodPais = 1;
+            provincia.CodProvincia = 1;
+            provincia.TxtDescripcion = "Pichincha";
+            provincias.Add(provincia);
+            Provincia provincia1 = new Provincia();
+            provincia1.CodPais = 1;
+            provincia1.CodProvincia = 2;
+            provincia1.TxtDescripcion = "Guayas";
+            provincias.Add(provincia1);
+            ViewBag.Provincias = provincias;
+
+            List<TipoDireccion> tiposDireccion = new List<TipoDireccion>();
+            TipoDireccion tipoDireccion = new TipoDireccion();
+            tipoDireccion.CodTipoDireccion = 1;
+            tipoDireccion.TxtDescripcion = "Domicilio";
+            tiposDireccion.Add(tipoDireccion);
+            ViewBag.TipoDireccion = tiposDireccion;
+
             return View();
         }
 
