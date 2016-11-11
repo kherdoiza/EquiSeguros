7fe60f11f7b8189c8e9fe4c6456f0d93d5289260
ElFer3107 <fernifm@gmail.com>
2016-11-11 17:20:17 -0500

    20161111 17:19
    Actualización AseguradoManager

--- EquiSeguros/Models/EntityManager/AseguradoManager.cs
+++ EquiSeguros/Models/EntityManager/AseguradoManager.cs
@@ -1,4 +1,5 @@
 ﻿using EquiSeguros.Models.BussinesEntity;
+using EquiSeguros.Models.DB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,44 @@ namespace EquiSeguros.Models.EntityManager
     {
         public void CrearAsegurado(Asegurado asegurado)
         {
-            //using (CotizadorEntitiesDb db = new CotizadorEntitiesDb())
-            //{
-            //    tmarca tmarca = new tmarca();
-            //    tmarca.cod_marca = marca.cod_marca;
-            //    tmarca.txt_desc = marca.txt_desc;
-            //    tmarca.txt_abrev = marca.txt_abrev;
-            //    db.tmarca.Add(tmarca);
-            //    db.SaveChanges();
-            //}
-            return;
+            using (Entities db = new Entities())
+            {
+                DB.Persona persona = new DB.Persona();
+                persona.Nombre = asegurado.Nombres;
+                persona.ApellidoPaterno = asegurado.ApellidoPaterno;
+                persona.ApellidoMaterno = asegurado.ApellidoMaterno;
+                persona.Documento = asegurado.Documento;
+                persona.TipoDocumento = asegurado.TipoDocumento;
+                persona.FechaNacimiento = asegurado.FechaNacimiento;
+                persona.TipoPersona = asegurado.TipoPersona;
+                persona.EstadoCivil = asegurado.EstadoCivil;
+                db.Persona.Add(persona);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Asegurado> ConsultaAsegurado()
+        {
+            using (Entities db = new Entities())
+            {
+                List<Asegurado> resultado = new List<Asegurado>();
+                List<DB.Persona> listaPersona = db.Persona.ToList();
+                listaPersona.ForEach(x =>
+                {
+                    Asegurado asegurado = new Asegurado();
+                    asegurado.Id = x.Id;
+                    asegurado.Nombres = x.Nombre;
+                    asegurado.ApellidoPaterno = x.ApellidoPaterno;
+                    asegurado.ApellidoMaterno = x.ApellidoMaterno;
+                    asegurado.Documento = x.Documento;
+                    asegurado.FechaNacimiento = x.FechaNacimiento;
+                    asegurado.TipoDocumento = x.TipoDocumento;
+                    asegurado.TipoPersona = x.TipoPersona;
+                    asegurado.EstadoCivil = x.EstadoCivil;
+                    resultado.Add(asegurado);
+                });
+                return resultado;
+            }
         }
 
     }
