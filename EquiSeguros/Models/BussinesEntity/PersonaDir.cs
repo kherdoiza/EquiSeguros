17c09baeedfbc418f30d4079453a22dceb17e62d
dannyepn <dannyepn@gmail.com>
2016-11-11 15:16:30 -0500

    version corregida

--- EquiSeguros/Models/BussinesEntity/PersonaDir.cs
+++ EquiSeguros/Models/BussinesEntity/PersonaDir.cs
@@ -7,5 +7,15 @@ namespace EquiSeguros.Models.BussinesEntity
 {
     public class PersonaDir
     {
+        public int IdPersona { get; set; }
+        public int CodTipoDir { get; set; }
+        public int CodPais { get; set; }
+        public int CodProvincia { get; set; }
+        public string TxtCallePrin { get; set; }
+        public string TxtCalleSec { get; set; }
+        public string Longitud { get; set; }
+        public string Latitud { get; set; }
+        public string TxtLugrReferencia { get; set; }
+
     }
 }
